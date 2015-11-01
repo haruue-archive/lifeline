2a8bf0e7daff76b60e399a8adc24b76502a2d668
zhuchenxi <973829691@qq.com>
2015-11-01 14:10:35 +0800

    修复没有一起死的bug

--- lifeline/src/com/jude/Main.java
+++ lifeline/src/com/jude/Main.java
@@ -7,17 +7,12 @@ import java.util.Map;
 public class Main {
     public static final Prisoner[] PRISONERS = {
             new NimaPrisoner(),
-            new NimaPrisoner(),
-            new NimaPrisoner(),
-            new NimaPrisoner(),
-            new NimaPrisoner(),
-            new NimaPrisoner(),
     };
 
 
     public static void main(String[] args) {
         //new 一个审判者，把囚徒们和1000颗豆子给他
-        Manager manager = new Manager(PRISONERS,1000);
+        Manager manager = new Manager(PRISONERS,10000);
         //审判1W次
         manager.start(3);
         for (Map.Entry<Prisoner, Integer> deciderIntegerEntry : manager.getScore().entrySet()) {
