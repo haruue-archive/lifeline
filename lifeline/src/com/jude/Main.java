ef9027b6ccbb1d3dfe01008769f5a74820f9d863
Haruue Icymoon <haruue@caoyue.com.cn>
2016-10-16 13:54:02 +0800

    re-initialize

--- lifeline/src/com/jude/Main.java
+++ lifeline/src/com/jude/Main.java
@@ -7,12 +7,17 @@ import java.util.Map;
 public class Main {
     public static final Prisoner[] PRISONERS = {
             new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
     };
 
 
     public static void main(String[] args) {
         //new 一个审判者，把囚徒们和1000颗豆子给他
-        Manager manager = new Manager(PRISONERS,10000);
+        Manager manager = new Manager(PRISONERS,1000);
         //审判1W次
         manager.start(10000);
         for (Map.Entry<Prisoner, Integer> deciderIntegerEntry : manager.getScore().entrySet()) {
