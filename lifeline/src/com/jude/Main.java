62e2b1c5008ac6605e9476fccbd4518bca0e8e6b
ReinWD <zhangw1998419@qq.com>
2016-10-28 17:05:00 +0800

    2016/10/28 final

--- lifeline/src/com/jude/Main.java
+++ lifeline/src/com/jude/Main.java
@@ -8,14 +8,19 @@ import java.util.Map;
 
 public class Main {
     public static final Prisoner[] PRISONERS = {
+            new AlexDeemPrisoner(),
+            new BlakePrisonerM(),       // Reflect
+            new GrantsomePrisoner(),    // Reflect
+            new HgsilPrisoner(),
+            new JayPrisoner(),          // Reflect
             new NimaPrisoner(),
-            new NimaPrisoner(),
-
-            new ZWPrisoner(),
-            new ZWPrisoner(),
-            new ZWPrisoner(),
-            new ZWPrisoner(),
-            new ZWPrisoner(),
+            new NookiaPrisoner(),       // Reflect
+            new PinkDPrisoner(),
+            new Rename(),               // Reflect
+            new XuanYuPrisoner(),
+            new ZhangPrisoner(),
+            new XiaossicaPrisoner(),
+            new NirioPrisoner(),
             new ZWPrisoner(),
     };
 
