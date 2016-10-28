f11c2cc7b986c3ca44fcdf67186873d4a3a99fa5
dishuang xu <1163965581@qq.com>
2016-10-28 21:14:30 +0800

    GodxuPrisoner.java

---
+++ lifeline/src/com/jude/GodxuPrisoner.java
@@ -0,0 +1,20 @@
+package com.jude;
+
+/**
+ * Created by hasee on 2016/10/28.
+ */
+class GodxuPrisoner  implements   Prisoner{int totalCount;int totalPerson;
+    public String getName(){
+        return "许弟霜2016210244";
+    }
+    public void begin(Manager manager,int totalCount,int totalPerson){
+        this.totalCount=totalCount;
+        this.totalPerson=totalPerson;
+
+    }
+    public int take(int index,int last){
+        //经过先知Godxu指引，这个数有天命在身。
+        return (totalPerson/totalCount+totalCount/totalPerson-30);
+    }public void result(boolean survive){
+    }}
+
