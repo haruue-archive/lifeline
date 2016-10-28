d63167ac5fcdf18483cd5ab0f8cb92e2a07eb430
huangxi1112 <aa346903657@qq.com>
2016-10-28 10:41:16 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.jude;
+
+import com.jude.prisoner.HxPrisoner;
+import com.jude.prisoner.JayPrisoner;
+import com.jude.prisoner.NimaPrisoner;
+import com.jude.prisoner.ZhangPrisoner;
+
+import java.util.Map;
+
+public class Main {
+    public static final Prisoner[] PRISONERS = {
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new NimaPrisoner(),
+            new JayPrisoner(),
+            new ZhangPrisoner(),
+            new HxPrisoner(),
+    };
+
+
+    public static void main(String[] args) {
+        //new 一个审判者，把囚徒们和1000颗豆子给他
+        Manager manager = new Manager(PRISONERS,1000);
+        //审判1W次
+        manager.start(10000);
+        for (Map.Entry<Prisoner, Integer> deciderIntegerEntry : manager.getScore().entrySet()) {
+            System.out.println(deciderIntegerEntry.getKey().getName()+":"+deciderIntegerEntry.getValue());
+        }
+    }
+
+}
